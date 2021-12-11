82e2444cd8288cec5d783430d1686704de0bdfc7
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-11 14:21:03 -0700

    Create test of basic call to parallel function

--- src/core_testing.py
+++ src/core_testing.py
@@ -1,1 +1,25 @@
 """Tests for core parapy functions"""
+
+import unittest
+
+import core
+
+
+class AnalysisLib(unittest.TestCase):
+    def test_parallel_basic(self):
+        """
+        Basic test of parallel functionality
+        """
+        # Setup
+        data = [
+            {'A': 0.0, 'B': 0.0, 'C': 0.0},
+            {'A': 1.0, 'B': 1.0, 'C': 1.0},
+            {'A': 2.0, 'B': 2.0, 'C': 2.0},
+        ]
+
+        # Run
+        fig = core.parallel(data=data)
+
+
+if __name__ == '__main__':
+    unittest.main()
