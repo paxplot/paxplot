37bef33073347262f5121e5533aec784248da0f0
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-11 15:21:36 -0700

    Create test for plotting a subset of columns

--- src/core_testing.py
+++ src/core_testing.py
@@ -21,6 +21,21 @@ class AnalysisLib(unittest.TestCase):
         fig = core.parallel(data=data, cols=['A', 'B', 'C'])
         fig.show()
 
+    def test_parallel_subset_columns(self):
+        """
+        Testing plotting a subset of all the columns
+        """
+        # Setup
+        data = [
+            {'A': 0.0, 'B': 0.0, 'C': 0.0, 'D': 0.0},
+            {'A': 1.0, 'B': 1.0, 'C': 1.0, 'D': 1.0},
+            {'A': 2.0, 'B': 2.0, 'C': 2.0, 'D': 2.0},
+        ]
+
+        # Run
+        fig = core.parallel(data=data, cols=['A', 'C', 'D'])
+        fig.show()
+
 
 if __name__ == '__main__':
     unittest.main()
