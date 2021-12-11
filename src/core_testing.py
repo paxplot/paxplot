fa1e4335b1a1f2beb2bd0beb5179317ace29474c
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-11 15:34:40 -0700

    Create test for plotting subset of columns with lines that cross

--- src/core_testing.py
+++ src/core_testing.py
@@ -36,6 +36,21 @@ class AnalysisLib(unittest.TestCase):
         fig = core.parallel(data=data, cols=['A', 'C', 'D'])
         fig.show()
 
+    def test_parallel_subset_columns_different(self):
+        """
+        Testing plotting a subset of all the columns with different values
+        """
+        # Setup
+        data = [
+            {'A': 0.0, 'B': 1.0, 'C': 0.0, 'D': 2.0},
+            {'A': 1.0, 'B': 1.0, 'C': 1.0, 'D': 1.0},
+            {'A': 2.0, 'B': 1.0, 'C': 2.0, 'D': 0.0},
+        ]
+
+        # Run
+        fig = core.parallel(data=data, cols=['A', 'B', 'D'])
+        fig.show()
+
 
 if __name__ == '__main__':
     unittest.main()
