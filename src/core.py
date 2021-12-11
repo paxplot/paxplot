4d76af3407723b5d53369828619680473fe07d9d
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-11 14:28:18 -0700

    Template of parallel function in core.py

--- src/core.py
+++ src/core.py
@@ -1,1 +1,28 @@
 """Core parapy functions"""
+
+
+def parallel(
+        data,
+        cols
+):
+    """
+    Create static parallel plot in Matplotlib.
+
+    Shouts to Been Alex Keen for giving example of underlying logic on their
+    website: https://benalexkeen.com/parallel-coordinates-in-matplotlib/
+
+    :param data: list
+        List of dictionaries containing the contents of data to be plotted.
+        This has the form:
+        [
+            {col1: val1, col2: val1 ...},
+            {col1: val2, col2: val2 ...}
+            ...
+        ]
+    :param cols: list
+        Columns to be plotted
+
+    :return:
+    """
+    fig = 0
+    return fig
