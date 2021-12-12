4e840c079a4d04f709af44c30ae3894a2b9f58ec
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-12 13:30:48 -0700

    Uncomment test_parappel_limits in core_testing.py

--- src/core_testing.py
+++ src/core_testing.py
@@ -66,30 +66,30 @@ class AnalysisLib(unittest.TestCase):
         fig = core.parallel(data=data, cols=['A', 'B', 'D'])
         fig.show()
 
-    # def test_parallel_limits(self):
-    #     """
-    #     Testing plotting a subset of all the columns with different values
-    #     """
-    #     # Setup
-    #     data = [
-    #         {'A': 0.0, 'B': 0.0, 'C': 0.0, 'D': 0.0},
-    #         {'A': 1.0, 'B': 1.0, 'C': 1.0, 'D': 1.0},
-    #         {'A': 2.0, 'B': 2.0, 'C': 2.0, 'D': 2.0},
-    #     ]
-    #     cols = ['A', 'B', 'D']
-    #     custom_lims = {
-    #         'A': [0, 10],
-    #         'B': [0, 1],
-    #         'D': [0, 5]
-    #     }
-    #
-    #     # Run
-    #     fig = core.parallel(
-    #         data=data,
-    #         cols=cols,
-    #         custom_lims=custom_lims
-    #     )
-    #     fig.show()
+    def test_parallel_limits(self):
+        """
+        Testing plotting a subset of all the columns with different values
+        """
+        # Setup
+        data = [
+            {'A': 0.0, 'B': 0.0, 'C': 0.0, 'D': 0.0},
+            {'A': 1.0, 'B': 1.0, 'C': 1.0, 'D': 1.0},
+            {'A': 2.0, 'B': 2.0, 'C': 2.0, 'D': 2.0},
+        ]
+        cols = ['A', 'B', 'D']
+        custom_lims = {
+            'A': [0, 10],
+            'B': [0, 1],
+            'D': [0, 5]
+        }
+
+        # Run
+        fig = core.parallel(
+            data=data,
+            cols=cols,
+            custom_lims=custom_lims
+        )
+        fig.show()
 
 
 if __name__ == '__main__':
