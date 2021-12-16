7ced5f64e0ce7841834d31f92b0a776cdc835e18
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-16 15:28:10 -0700

    Create test for creating colorbar

--- src/core_testing.py
+++ src/core_testing.py
@@ -131,6 +131,26 @@ class AnalysisLib(unittest.TestCase):
         )
         fig.show()
 
+    def test_parallel_colorbar(self):
+        """
+        Testing colorbar
+        """
+        # Setup
+        data = [
+            {'A': 0.0, 'B': 0.0, 'C': 0.0},
+            {'A': 1.0, 'B': 1.0, 'C': 1.0},
+            {'A': 2.0, 'B': 2.0, 'C': 2.0},
+        ]
+
+        # Run
+        fig = core.parallel(
+            data=data,
+            cols=['A', 'B', 'C'],
+            color_col='A',
+            colorbar=True
+        )
+        fig.show()
+
 
 if __name__ == '__main__':
     unittest.main()
