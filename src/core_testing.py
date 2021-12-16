520c80f857d4f17bd363ec663c0fdd7bcbca57d8
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-16 14:06:27 -0700

    Write test to show custom colormap functionality

--- src/core_testing.py
+++ src/core_testing.py
@@ -102,13 +102,32 @@ class AnalysisLib(unittest.TestCase):
             {'A': 1.0, 'B': 1.0, 'C': 1.0},
             {'A': 2.0, 'B': 2.0, 'C': 2.0},
         ]
-        color_col = 'A'
 
         # Run
         fig = core.parallel(
             data=data,
             cols=['A', 'B', 'C'],
-            color_col=color_col
+            color_col='A'
+        )
+        fig.show()
+
+    def test_parallel_custom_color(self):
+        """
+        Testing color custom column
+        """
+        # Setup
+        data = [
+            {'A': 0.0, 'B': 0.0, 'C': 0.0},
+            {'A': 1.0, 'B': 1.0, 'C': 1.0},
+            {'A': 2.0, 'B': 2.0, 'C': 2.0},
+        ]
+
+        # Run
+        fig = core.parallel(
+            data=data,
+            cols=['A', 'B', 'C'],
+            color_col='A',
+            color_col_colormap='plasma'
         )
         fig.show()
 
