86152871f817fc91defdf93f7f1427cfbee1d4c8
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-17 13:56:06 -0700

    Write test for inverting columns

--- src/core_testing.py
+++ src/core_testing.py
@@ -222,6 +222,25 @@ class AnalysisLib(unittest.TestCase):
         )
         fig.show()
 
+    def test_parallel_invert(self):
+        """
+        Test for inverting columns
+        """
+        # Setup
+        data = [
+            {'A': 0.0, 'B': 0.0, 'C': 0.0},
+            {'A': 1.0, 'B': 1.0, 'C': 1.0},
+            {'A': 2.0, 'B': 2.0, 'C': 2.0},
+        ]
+
+        # Run
+        fig = core.parallel(
+            data=data,
+            cols=['A', 'B', 'C'],
+            cols_invert=['A', 'B'],
+        )
+        fig.show()
+
 
 if __name__ == '__main__':
     unittest.main()
