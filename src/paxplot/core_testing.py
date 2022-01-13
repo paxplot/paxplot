55e7f3b07a280ea2d89046b8c732e6d6daa79fb0
Jacob Kravits <kravitsjacob@gmail.com>
2022-01-12 22:43:56 -0700

    Move to attribute-based testing

--- src/paxplot/core_testing.py
+++ src/paxplot/core_testing.py
@@ -2,10 +2,6 @@
 
 import unittest
 import os
-import random
-from string import ascii_lowercase
-import numpy as np
-import matplotlib.pyplot as plt
 
 import core
 
@@ -17,17 +13,34 @@ class AnalysisLib(unittest.TestCase):
         """
         # Run
         fig, axes = core.pax_parallel(n_axes=4)
-        fig.savefig('temp.png')
 
         # Test
-        image = open('temp.png', 'rb')
-        image_test = open('test_solution_images/test_parallel_blank.png', 'rb')
-        image_contents = image.read()
-        image_test_contents = image_test.read()
-        image.close()
-        image_test.close()
-        os.remove('temp.png')
-        self.assertEqual(image_contents, image_test_contents)
+        fig.show()
+        self.assertEqual(fig.axes.__len__(), 4)
+        self.assertEqual(
+            fig.subplotpars.__getattribute__('wspace'),
+            0.0
+        )
+        self.assertEqual(
+            fig.subplotpars.__getattribute__('hspace'),
+            0.0
+        )
+        for ax in fig.axes:
+            self.assertEqual(
+                ax.spines['top'].get_visible(),
+                False
+            ),
+            self.assertEqual(
+                ax.spines['bottom'].get_visible(),
+                False
+            )
+            self.assertEqual(
+                ax.spines['right'].get_visible(),
+                False
+            )
+            self.assertEqual(ax.get_ylim(), (0.0, 1.0))
+            self.assertEqual(ax.get_xlim(), (0.0, 1.0))
+            self.assertEqual(ax.get_xticks(), [0])
 
     def test_parallel_basic(self):
         """
@@ -43,17 +56,9 @@ class AnalysisLib(unittest.TestCase):
         # Run
         fig, axes = core.pax_parallel(n_axes=len(data[0]))
         axes.plot(data)
-        fig.savefig('temp.png')
 
         # Test
-        image = open('temp.png', 'rb')
-        image_test = open('test_solution_images/test_parallel_basic.png', 'rb')
-        image_contents = image.read()
-        image_test_contents = image_test.read()
-        image.close()
-        image_test.close()
-        os.remove('temp.png')
-        self.assertEqual(image_contents, image_test_contents)
+        fig.show()
 
 
 if __name__ == '__main__':
