bd0abe27f1e0e93a8bdfe6674b79a6b0d3157d30
Jacob Kravits <kravitsjacob@gmail.com>
2022-01-13 23:15:23 -0700

    Add test for custom limits on axis

--- src/paxplot/core_testing.py
+++ src/paxplot/core_testing.py
@@ -76,6 +76,53 @@ class AnalysisLib(unittest.TestCase):
             paxes.axes[0].get_yticklabels()[-1].get_position()[1], 1.0
         )
 
+    def test_parallel_limits(self):
+        """
+        Testing plotting with custom limits
+        """
+        # Setup
+        data = [
+            [0.0, 0.0, 0.0],
+            [1.0, 1.0, 1.0],
+            [2.0, 2.0, 2.0]
+        ]
+
+        # Run
+        paxfig, paxes = core.pax_parallel(n_axes=len(data[0]))
+        paxes.plot(data)
+        paxes.set_ylim(paxes.axes[0], bottom=-1.0, top=3)
+        paxes.set_ylim(paxes.axes[2], bottom=0.0, top=1.5)
+
+        # Test plotted data
+        self.assertEqual(paxes.axes[0].lines[0].get_ydata()[0], 0.25)
+        self.assertEqual(paxes.axes[0].lines[1].get_ydata()[0], 0.50)
+        self.assertEqual(paxes.axes[0].lines[2].get_ydata()[0], 0.75)
+        self.assertEqual(paxes.axes[1].lines[0].get_ydata()[1], 0.0, 1)
+        self.assertAlmostEqual(
+            paxes.axes[1].lines[1].get_ydata()[1], 0.6666, 2
+        )
+        self.assertAlmostEqual(
+            paxes.axes[1].lines[2].get_ydata()[1], 1.3333, 2
+        )
+
+        # Test ticks
+        self.assertEqual(paxes.axes[0].get_yticklabels()[0].get_text(), '-1.0')
+        self.assertEqual(
+            paxes.axes[0].get_yticklabels()[0].get_position()[1], 0.0
+        )
+        self.assertEqual(paxes.axes[0].get_yticklabels()[-1].get_text(), '3.0')
+        self.assertEqual(
+            paxes.axes[0].get_yticklabels()[-1].get_position()[1], 1.0
+        )
+        self.assertEqual(paxes.axes[2].get_yticklabels()[0].get_text(), '0.0')
+        self.assertEqual(
+            paxes.axes[2].get_yticklabels()[0].get_position()[1], 0.0
+        )
+        self.assertEqual(paxes.axes[2].get_yticklabels()[-1].get_text(), '1.5')
+        self.assertEqual(
+            paxes.axes[2].get_yticklabels()[-1].get_position()[1], 1.0
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
