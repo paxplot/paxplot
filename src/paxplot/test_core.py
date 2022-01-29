1998d13dff924dbe286f2ebf44e2a26f77308f07
Jacob Kravits <kravitsjacob@gmail.com>
2022-01-29 14:28:33 -0700

    Update test_parallel_ticks to single object API

--- src/paxplot/test_core.py
+++ src/paxplot/test_core.py
@@ -129,7 +129,7 @@ class PaxplotLib(unittest.TestCase):
             paxfig.axes[2].get_yticklabels()[-1].get_position()[1], 1.0
         )
 
-    def test_parallel_yticks(self):
+    def test_parallel_ticks(self):
         """
         Testing plotting with modified ticks
         """
@@ -141,27 +141,27 @@ class PaxplotLib(unittest.TestCase):
         ]
 
         # Run
-        paxfig, paxes = core.pax_parallel(n_axes=len(data[0]))
-        paxes.plot(data)
-        paxes.set_yticks(paxes.axes[0], ticks=[0.0, 1.0, 2.0])
-        paxes.set_yticks(paxes.axes[1], ticks=[0.0, 0.5, 1.0, 2.0, 2.5])
+        paxfig = core.pax_parallel(n_axes=len(data[0]))
+        paxfig.plot(data)
+        paxfig.set_ticks(ax_idx=0, ticks=[0.0, 1.0, 2.0])
+        paxfig.set_ticks(ax_idx=1, ticks=[0.0, 0.5, 1.0, 2.0, 2.5])
 
         # Test ticks
-        self.assertEqual(paxes.axes[0].get_yticklabels()[0].get_text(), '0.0')
-        self.assertEqual(paxes.axes[0].get_yticklabels()[2].get_text(), '2.0')
-        self.assertEqual(paxes.axes[1].get_yticklabels()[0].get_text(), '0.0')
-        self.assertEqual(paxes.axes[1].get_yticklabels()[4].get_text(), '2.5')
+        self.assertEqual(paxfig.axes[0].get_yticklabels()[0].get_text(), '0.0')
+        self.assertEqual(paxfig.axes[0].get_yticklabels()[2].get_text(), '2.0')
+        self.assertEqual(paxfig.axes[1].get_yticklabels()[0].get_text(), '0.0')
+        self.assertEqual(paxfig.axes[1].get_yticklabels()[4].get_text(), '2.5')
         self.assertEqual(
-            paxes.axes[0].get_yticklabels()[0].get_position()[1], 0.0
+            paxfig.axes[0].get_yticklabels()[0].get_position()[1], 0.0
         )
         self.assertEqual(
-            paxes.axes[0].get_yticklabels()[2].get_position()[1], 1.0
+            paxfig.axes[0].get_yticklabels()[2].get_position()[1], 1.0
         )
         self.assertEqual(
-            paxes.axes[1].get_yticklabels()[0].get_position()[1], 0.0
+            paxfig.axes[1].get_yticklabels()[0].get_position()[1], 0.0
         )
         self.assertEqual(
-            paxes.axes[1].get_yticklabels()[4].get_position()[1], 1.0
+            paxfig.axes[1].get_yticklabels()[4].get_position()[1], 1.0
         )
 
     def test_parallel_custom_yticks(self):
