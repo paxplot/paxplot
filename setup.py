db50a11cdc8051ec14cffa6d175ceb43f20d2a48
Jacob Kravits <kravitsjacob@gmail.com>
2022-02-19 18:53:34 -0700

    Update setup versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.1.dev0",
     author="Jacob Kravits",
     author_email="kravitsjacob@gmail.com",
-    description="Create static parallel axis plots in Python",
+    description="Paxplot is a Python visualization library for parallel coordinate plots based on matplotlib.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kravitsjacob/paxplot",
@@ -16,8 +16,7 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/kravitsjacob/paxplot/issues",
     },
     install_requires=[
-        'numpy>=1.16',
-        'matplotlib>=3.5',
+        'matplotlib',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -26,7 +25,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     include_package_data=True,
     package_data={'': ['data/tradeoff.csv']},
 )
