cb1bc60bbebb658f3eed7a8e44d84975d39200ec
Jacob Kravits <kravitsjacob@gmail.com>
2022-02-26 20:13:57 -0700

    Update version tag

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Jacob Kravits'
 author = 'Jacob Kravits'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.0'
+release = 'v0.1.1-beta'
 
 
 # -- General configuration ---------------------------------------------------
