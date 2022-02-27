f1ce5145d154c12ef289ae09eed6223975f41c89
Jacob Kravits <kravitsjacob@gmail.com>
2022-02-26 21:08:26 -0700

    Change to beta tag

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Jacob Kravits'
 author = 'Jacob Kravits'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.1-beta'
+release = 'beta'
 
 
 # -- General configuration ---------------------------------------------------
